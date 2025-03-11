a74a6db6aadce7c71c39fcd74296bde95dc326ef
Jakub Birecki <kubabir@outlook.com>
2025-03-11 20:46:10 +0100

    .

--- frontend/src/features/auth/AuthApi.jsx
+++ frontend/src/features/auth/AuthApi.jsx
@@ -1,66 +1,66 @@
-import {axiosi} from '../../config/axios'
+import { axiosi } from '../../config/axios';
 
-export const signup=async(cred)=>{
+export const signup = async (cred) => {
     try {
-        const res=await axiosi.post("auth/signup",cred)
-        return res.data
+        const res = await axiosi.post('auth/signup', cred);
+        return res.data;
     } catch (error) {
-        throw error.response.data
+        throw error.response.data;
     }
-}
-export const login=async(cred)=>{
+};
+export const login = async (cred) => {
     try {
-        const res=await axiosi.post("auth/login",cred)
-        return res.data
+        const res = await axiosi.post('auth/login', cred);
+        return res.data;
     } catch (error) {
-        throw error.response.data
+        throw error.response.data;
     }
-}
-export const verifyOtp=async(cred)=>{
+};
+export const verifyOtp = async (cred) => {
     try {
-        const res=await axiosi.post("auth/verify-otp",cred)
-        return res.data
+        const res = await axiosi.post('auth/verify-otp', cred);
+        return res.data;
     } catch (error) {
-        throw error.response.data
+        throw error.response.data;
     }
-}
-export const resendOtp=async(cred)=>{
+};
+export const resendOtp = async (cred) => {
     try {
-        const res=await axiosi.post("auth/resend-otp",cred)
-        return res.data
+        const res = await axiosi.post('auth/resend-otp', cred);
+        return res.data;
     } catch (error) {
-        throw error.response.data
+        throw error.response.data;
     }
-}
-export const forgotPassword=async(cred)=>{
+};
+export const forgotPassword = async (cred) => {
     try {
-        const res=await axiosi.post("auth/forgot-password",cred)
-        return res.data
+        const res = await axiosi.post('auth/forgot-password', cred);
+        return res.data;
     } catch (error) {
-        throw error.response.data
+        throw error.response.data;
     }
-}
-export const resetPassword=async(cred)=>{
+};
+export const resetPassword = async (cred) => {
     try {
-        const res=await axiosi.post("auth/reset-password",cred)
-        return res.data
+        const res = await axiosi.post('auth/reset-password', cred);
+        return res.data;
     } catch (error) {
-        throw error.response.data
+        throw error.response.data;
     }
-}
-export const checkAuth=async(cred)=>{
+};
+export const checkAuth = async (cred) => {
     try {
-        const res=await axiosi.get("auth/check-auth")
-        return res.data
+        const res = await axiosi.get('auth/check-auth');
+        return res.data;
     } catch (error) {
-        throw error.response.data
+        throw error.response.data;
     }
-}
-export const logout=async()=>{
+};
+export const logout = async () => {
     try {
-        const res=await axiosi.get("auth/logout")
-        return res.data
+        const res = await axiosi.get('auth/logout');
+        return res.data;
     } catch (error) {
-        throw error.response.data
+        throw error.response.data;
     }
-}
+};
